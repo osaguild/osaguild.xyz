9530161204ae9784cb78bf3ee1a9381c2327fcdc
osaguild <nobuhisa.abe.0922@gmail.com>
2023-04-02 16:45:28 +0900

    add document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,18 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 const Document = () => {
-	return (
-		<Html lang="en">
-			<Head>
-				<title>osaguild.xyz</title>
-				<meta name="description" content="this is my home. thanks to visit." />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<body>
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	);
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="description" content="this is my home. thanks to visit." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 };
 
 export default Document;
