412de614e7c767311fdabe81b5f56f24fa572dec
osaguild <nobuhisa.abe.0922@gmail.com>
2023-04-02 16:45:42 +0900

    add header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,72 @@
 import dynamic from "next/dynamic";
+import {
+  Box,
+  Flex,
+  Heading,
+  Tabs,
+  TabList,
+  Tab,
+  TabIndicator,
+  TabPanels,
+  TabPanel,
+  HStack,
+  Link,
+} from "@chakra-ui/react";
+import { FaGithub, FaTwitter } from "react-icons/fa";
+import Head from "next/head";
 
 const DynamicTerminal = dynamic(() => import("@/components/Terminal"), {
-	ssr: false,
+  ssr: false,
 });
 
-const Top = () => {
-	return <DynamicTerminal />;
-};
+function Top() {
+  return (
+    <>
+      <Head>
+        <title>osaguild.xyz</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Box>
+        <Flex
+          as="header"
+          alignItems="flex-end"
+          justifyContent="space-between"
+          padding="1rem"
+          borderBottom="1px solid"
+          borderColor="gray.200"
+        >
+          <Heading as="h1" size="lg" mb="10px">
+            osaguild.xyz
+          </Heading>
+          <Tabs w="100%" variant="unstyled" ml="30px">
+            <TabList justifyContent="flex-start">
+              <Tab>about</Tab>
+              <Tab>products</Tab>
+              <Tab>contact</Tab>
+            </TabList>
+            <TabIndicator
+              mt="-1.5px"
+              height="2px"
+              bg="blue.500"
+              borderRadius="1px"
+            />
+          </Tabs>
+          <HStack spacing={4}>
+            <Link href="https://github.com/osaguild" isExternal>
+              <FaGithub size="30" />
+            </Link>
+            <Link href="https://twitter.com/osaguild" isExternal>
+              <FaTwitter size="30" />
+            </Link>
+          </HStack>
+        </Flex>
+
+        <Box as="main" padding="1rem">
+          <DynamicTerminal />
+        </Box>
+      </Box>
+    </>
+  );
+}
 
 export default Top;
