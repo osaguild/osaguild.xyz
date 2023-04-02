d056473222fa49b7b2e8dffde76dcf9f5bfd4f1c
osaguild <nobuhisa.abe.0922@gmail.com>
2023-04-02 19:03:17 +0900

    move tab to layout

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,5 +1,17 @@
 import { FunctionComponent } from "react";
 import Head from "next/head";
+import {
+  Box,
+  Flex,
+  Tabs,
+  TabList,
+  Tab,
+  TabIndicator,
+  HStack,
+  Link,
+  Image,
+} from "@chakra-ui/react";
+import { FaGithub, FaTwitter } from "react-icons/fa";
 
 interface LayoutProps {
   children?: React.ReactNode;
@@ -7,10 +19,46 @@ interface LayoutProps {
 
 const Layout: FunctionComponent<LayoutProps> = ({ children }) => {
   return (
-    <Head>
-      <title>osaguild.xyz</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </Head>
+    <>
+      <Head>
+        <title>osaguild.xyz</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Box>
+        <Flex
+          as="header"
+          alignItems="flex-end"
+          justifyContent="space-between"
+          padding="1rem"
+          borderBottom="1px solid"
+          borderColor="gray.200"
+        >
+          <Image src="/osa.png" alt="osa" w="200px" />
+          <Tabs w="100%" variant="unstyled" ml="30px">
+            <TabList justifyContent="flex-start">
+              <Tab>about</Tab>
+              <Tab>products</Tab>
+              <Tab>contact</Tab>
+            </TabList>
+            <TabIndicator
+              mt="-1.5px"
+              height="2px"
+              bg="blue.500"
+              borderRadius="1px"
+            />
+          </Tabs>
+          <HStack spacing={4}>
+            <Link href="https://github.com/osaguild" isExternal>
+              <FaGithub size="30" />
+            </Link>
+            <Link href="https://twitter.com/osaguild" isExternal>
+              <FaTwitter size="30" />
+            </Link>
+          </HStack>
+        </Flex>
+        {children}
+      </Box>
+    </>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,4 @@
 import dynamic from "next/dynamic";
-import {
-  Box,
-  Flex,
-  Heading,
-  Tabs,
-  TabList,
-  Tab,
-  TabIndicator,
-  HStack,
-  Link,
-  Image,
-} from "@chakra-ui/react";
-import { FaGithub, FaTwitter } from "react-icons/fa";
 import Layout from "@/components/Layout";
 
 const DynamicTerminal = dynamic(() => import("@/components/Terminal/index"), {
@@ -20,46 +7,9 @@ const DynamicTerminal = dynamic(() => import("@/components/Terminal/index"), {
 
 function Top() {
   return (
-    <>
-      <Layout />
-      <Box>
-        <Flex
-          as="header"
-          alignItems="flex-end"
-          justifyContent="space-between"
-          padding="1rem"
-          borderBottom="1px solid"
-          borderColor="gray.200"
-        >
-          <Image src="/osa.png" alt="osa" w="200px" />
-          <Tabs w="100%" variant="unstyled" ml="30px">
-            <TabList justifyContent="flex-start">
-              <Tab>about</Tab>
-              <Tab>products</Tab>
-              <Tab>contact</Tab>
-            </TabList>
-            <TabIndicator
-              mt="-1.5px"
-              height="2px"
-              bg="blue.500"
-              borderRadius="1px"
-            />
-          </Tabs>
-          <HStack spacing={4}>
-            <Link href="https://github.com/osaguild" isExternal>
-              <FaGithub size="30" />
-            </Link>
-            <Link href="https://twitter.com/osaguild" isExternal>
-              <FaTwitter size="30" />
-            </Link>
-          </HStack>
-        </Flex>
-
-        <Box as="main" padding="1rem">
-          <DynamicTerminal />
-        </Box>
-      </Box>
-    </>
+    <Layout>
+      <DynamicTerminal />
+    </Layout>
   );
 }
 
