6a29da0ed504c8b2076bbbc3de259f7e793d6396
osaguild <nobuhisa.abe.0922@gmail.com>
2023-04-02 15:10:55 +0900

    show terminal

---
+++ components/Terminal.tsx
@@ -0,0 +1,40 @@
+import { FC, useRef, useEffect } from "react";
+import { Terminal } from "xterm";
+import { FitAddon } from "xterm-addon-fit";
+import "xterm/css/xterm.css";
+
+const TerminalComponent: FC = () => {
+	const terminalRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const terminalInstance = new Terminal();
+		const fitAddon = new FitAddon();
+		terminalInstance.loadAddon(fitAddon);
+
+		if (terminalRef.current) {
+			terminalInstance.open(terminalRef.current);
+			fitAddon.fit();
+		}
+
+		terminalInstance.writeln("Hello, World!");
+
+		// Add command handling
+		terminalInstance.onData((data) => {
+			const code = data.charCodeAt(0);
+			if (code === 13) {
+				// Enter key
+				terminalInstance.write("\r\n");
+			} else {
+				terminalInstance.write(data);
+			}
+		});
+
+		return () => {
+			terminalInstance.dispose();
+		};
+	}, []);
+
+	return <div ref={terminalRef} style={{ width: "500px", height: "500px" }} />;
+};
+
+export default TerminalComponent;
