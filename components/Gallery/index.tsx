0f3f7bafe4a82d5b9e81ae1171988bf9a8719579
osaguild <nobuhisa.abe.0922@gmail.com>
2023-04-05 23:04:10 +0900

    add pics to gallery

--- components/Gallery/index.tsx
+++ components/Gallery/index.tsx
@@ -1,12 +1,13 @@
 import { FC } from "react";
 import { Box } from "@chakra-ui/react";
 import RandomImage from "./RandomImage";
+import { pics } from "@/consts";
 
 const Gallery: FC = () => {
   return (
     <Box w="100px" h="100px">
-      {Array.from({ length: 16 }, (_, i) => (
-        <RandomImage key={i} path={"/osa.png"} />
+      {pics.map((e, i) => (
+        <RandomImage key={i} path={e} />
       ))}
     </Box>
   );
--- consts/index.ts
+++ consts/index.ts
@@ -1,14 +1,32 @@
 const tabs = [
-	{
-		label: "hello",
-		image: "/hello.png",
-		path: "/hello",
-	},
-	{
-		label: "dot",
-		image: "/dot.png",
-		path: "/dot",
-	},
+  {
+    label: "hello",
+    image: "/tabs/hello.png",
+    path: "/hello",
+  },
+  {
+    label: "dot",
+    image: "/tabs/dot.png",
+    path: "/dot",
+  },
 ];
 
-export { tabs };
+const pics = [
+  "/images/ai.png",
+  "/images/app.png",
+  "/images/art.png",
+  "/images/awesome.png",
+  "/images/dapp.png",
+  "/images/deep.png",
+  "/images/dev.png",
+  "/images/doop.png",
+  "/images/geek.png",
+  "/images/goal.png",
+  "/images/me.png",
+  "/images/music.png",
+  "/images/web.png",
+  "/images/www.png",
+  "/images/z.png",
+];
+
+export { tabs, pics };
