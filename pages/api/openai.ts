03c09b05a95f78cd4e166de37f6468d44cdd2605
osaguild <nobuhisa.abe.0922@gmail.com>
2023-04-30 16:35:20 +0900

    add to call openai

---
+++ pages/api/openai.ts
@@ -0,0 +1,34 @@
+import { Configuration, OpenAIApi } from "openai";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<{ message: string }>
+) {
+  const configuration = new Configuration({
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      message: "OpenAI API key not configured",
+    });
+    return;
+  }
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: req.body.message,
+      temperature: 1,
+      max_tokens: 1000,
+    });
+    res.status(200).json({ message: completion.data.choices[0].text!.trim() });
+  } catch (e) {
+    console.error(`Error with OpenAI API request: ${e}`);
+    res.status(500).json({
+      message: "An error occurred during your request.",
+    });
+  }
+}
--- src/components/Terminal/index.tsx
+++ src/components/Terminal/index.tsx
@@ -23,26 +23,49 @@ const Terminal: FC = () => {
 
     terminalInstance.onData((data) => {
       const code = data.charCodeAt(0);
+
+      // if enter key is pressed
       if (code === 13) {
-        // Enter key
         terminalInstance.write("\r\n");
 
-        // Check if inputBuffer contains "hello"
-        if (inputBuffer.trim() === "hello") {
-          terminalInstance.writeln(
-            "hello. thanks to visit my home. but, I'm under development. please wait a moment."
-          );
-        } else {
-          terminalInstance.writeln(
-            "please type 'hello'. I want to talk to you."
-          );
-        }
+        // call api with inputBuffer
+        fetch("/api/openai", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ message: inputBuffer.trim() }),
+        })
+          .then((res) => {
+            if (res.status === 200) {
+              return res.json();
+            } else {
+              throw new Error(`failed to call api. http status: ${res.status}`);
+            }
+          })
+          // if success
+          .then((data) => {
+            // clear inputBuffer
+            inputBuffer = "";
 
-        // Clear inputBuffer
-        inputBuffer = "";
-        terminalInstance.write("> ");
+            // show response
+            terminalInstance.writeln(data.message);
+            terminalInstance.write("> ");
+          })
+          // if failed
+          .catch((err) => {
+            // print error
+            console.error(err);
+
+            // Clear inputBuffer
+            inputBuffer = "";
+
+            // show error message
+            terminalInstance.writeln("error occurred. please try again.");
+            terminalInstance.write("> ");
+          });
+        // if backspace key is pressed
       } else if (code === 8 || code === 127) {
-        // Backspace key
         if (inputBuffer.length > 0) {
           terminalInstance.write("\b \b");
           inputBuffer = inputBuffer.slice(0, -1);
