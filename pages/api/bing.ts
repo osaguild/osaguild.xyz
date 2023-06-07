cf3edc0526bfbef2bb276868fddb01838b3cadbc
osaguild <nobuhisa.abe.0922@gmail.com>
2023-06-07 18:45:25 +0900

    return html body

--- pages/api/bing.ts
+++ pages/api/bing.ts
@@ -1,13 +1,19 @@
 import BingApi from "@/libs/bing";
 import type { NextApiRequest, NextApiResponse } from "next";
+import cheerio from "cheerio";
 
 const handler = async (
   req: NextApiRequest,
   res: NextApiResponse<{ message: string }>
 ) => {
   try {
-    const bingResponse = await BingApi.getInstance().search(req.body.query);
-    res.status(200).json({ message: JSON.stringify(bingResponse) });
+    const bingRes = await BingApi.getInstance().search(req.body.query);
+    const fetchRes = await fetch(bingRes.webPages.value[0].url);
+    const html = await fetchRes.text();
+    const $ = cheerio.load(html);
+    const body = $("body").html()!;
+
+    res.status(200).json({ message: body });
   } catch (e) {
     res.status(500).json({
       message: "An error occurred during your request.",
