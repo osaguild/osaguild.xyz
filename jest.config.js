9a296bd4c7cdccf4e6597c3e01cda9855793ac5a
osaguild <nobuhisa.abe.0922@gmail.com>
2023-06-10 16:35:54 +0900

    remove test package

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
-    "^@test/(.*)$": "<rootDir>/test/$1",
-  },
-};
